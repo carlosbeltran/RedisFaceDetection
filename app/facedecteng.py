b1a18a54472675eaffb7f66324e38ad9eb52b868
carlos <cbeltran@libero.it>
2020-04-04 22:38:30 +0200

    Adding the haar based face and eyes detection.

--- app/facedecteng.py
+++ app/facedecteng.py
@@ -5,6 +5,8 @@ import cv2
 import numpy as np
 from PIL import Image
 
+face_cascade = cv2.CascadeClassifier('../../FaceDetection/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('../../FaceDetection/haarcascade_eye.xml')
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 ps = r.pubsub()
@@ -22,9 +24,28 @@ while True:
             data = io.BytesIO(simg[0][1][0][1][b'image'])
         except:
             pass
-        img = Image.open(data)
-        arr = np.array(img)
-        arr = cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
+        img  = Image.open(data)
+        arr  = np.array(img)
+        arr  = cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
+        gray = cv2.cvtColor(arr, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray)
+
+        boxes = []
+        counter = 0
+
+        for (x,y,w,h) in faces:
+           counter +=1
+           cv2.rectangle(arr,(x,y),(x+w,y+h),(255,0,0),2)
+           # font=cv2.FONT_HERSHEY_SIMPLEX
+           #cv2.putText(img,'Jeevy',(x+w/2,y+h/2),font,0.5,(0,0,255),1,cv2.LINE_AA)
+           #print(x,y)
+           roi_gray  = gray[y:y+h, x:x+w]
+           roi_color = arr[y:y+h, x:x+w]
+           eyes=eye_cascade.detectMultiScale(roi_gray)
+           # smiles=smile_cascade.detectMultiScale(roi_gray)
+           for(ex,ey,ew,eh) in eyes:
+               cv2.rectangle(roi_color,(ex, ey),(ex+ew, ey+eh),(0,255,0),2)
+
         name = 'outputtest'+str(count)+'.jpg'
         cv2.imwrite(name,arr)
         count += 1
