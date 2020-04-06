351bc1a15591363303c44144b0bba121b13f103c
carlos <cbeltran@libero.it>
2020-04-06 15:37:15 +0200

    Making the blocking calls directly with xread block option
    Instead of using the pub/sub mechanism lets use directly xreads with blocking option

--- app/capture.py
+++ app/capture.py
@@ -2,7 +2,8 @@ import redis
 import cv2
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-
+#ps = r.pubsub()
+#ps.subscribe('boxes')
 img=cv2.imread('../girl_small.jpg')
 
 count = 1
@@ -13,5 +14,7 @@ msg = {
 }
 count += 1
 _id = r.xadd('camera:0', msg, maxlen=10000)
+boxes = r.xread({'camera:0:facedect':'$'},count=1,block=0)
 
-r.publish('images',_id)
+print(boxes[0][1][0][1][b'facesboxes'])
+print(boxes[0][1][0][1][b'eyesboxes'])
--- app/facedecteng.py
+++ app/facedecteng.py
@@ -6,55 +6,54 @@ import numpy as np
 from PIL import Image
 
 face_cascade = cv2.CascadeClassifier('../../FaceDetection/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('../../FaceDetection/haarcascade_eye.xml')
+eye_cascade  = cv2.CascadeClassifier('../../FaceDetection/haarcascade_eye.xml')
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-ps = r.pubsub()
-ps.subscribe('images')
-#new_msg = ps.get_message()['data']
-#print (new_msg)
+
 count = 0
 while True:
-    for message in ps.listen():
-        print(message)
-        img_id = message['data']
-        simg = r.xread({'camera:0':img_id})
-        #print(simg)
-        try:
-            data = io.BytesIO(simg[0][1][0][1][b'image'])
-        except:
-            pass
-        img  = Image.open(data)
-        arr  = np.array(img)
-        arr  = cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
-        gray = cv2.cvtColor(arr, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray)
 
-        boxes = []
-        counter = 0
+    simg = r.xread({'camera:0':'$'},count=1,block=0)
+    try:
+        img_id = simg[0][1][0][0]
+        print(img_id)
+        data = io.BytesIO(simg[0][1][0][1][b'image'])
+    except:
+        pass
+    img   = Image.open(data)
+    arr   = np.array(img)
+    arr   = cv2.cvtColor(arr, cv2.COLOR_BGR2RGB)
+    gray  = cv2.cvtColor(arr, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
 
-        for (x,y,w,h) in faces:
-           counter +=1
-           cv2.rectangle(arr,(x,y),(x+w,y+h),(255,0,0),2)
-           boxes += [int(x), int(y), int(x+w), int(y+h)]
-           # font=cv2.FONT_HERSHEY_SIMPLEX
-           #cv2.putText(img,'Jeevy',(x+w/2,y+h/2),font,0.5,(0,0,255),1,cv2.LINE_AA)
-           #print(x,y)
-           roi_gray  = gray[y:y+h, x:x+w]
-           roi_color = arr[y:y+h, x:x+w]
-           eyes=eye_cascade.detectMultiScale(roi_gray)
-           # smiles=smile_cascade.detectMultiScale(roi_gray)
-           for(ex,ey,ew,eh) in eyes:
-               cv2.rectangle(roi_color,(ex, ey),(ex+ew, ey+eh),(0,255,0),2)
+    facesboxes = []
+    eyesboxes  = []
+    counter    = 0
 
-        if boxes:
-            msg = {
-                'boxes': str(boxes),
-                'ref_id':img_id
-            }
-            _id = r.xadd('camera:0:facedect',msg)
+    for (x,y,w,h) in faces:
+       counter +=1
 
-        name = 'outputtest'+str(count)+'.jpg'
-        cv2.imwrite(name,arr)
-        count += 1
+       cv2.rectangle(arr,(x,y),(x+w,y+h),(255,0,0),2)
+       facesboxes += [int(x), int(y), int(x+w), int(y+h)]
+
+       roi_gray  = gray[y:y+h, x:x+w]
+       roi_color = arr[y:y+h, x:x+w]
+
+       eyes=eye_cascade.detectMultiScale(roi_gray)
+
+       for(ex,ey,ew,eh) in eyes:
+           eyesboxes += [int(ex), int(ey), int(ex+ew), int(ey+eh)]
+           cv2.rectangle(roi_color,(ex, ey),(ex+ew, ey+eh),(0,255,0),2)
+
+    if facesboxes:
+        msg = {
+            'facesboxes': str(facesboxes),
+            'eyesboxes' : str(eyesboxes),
+            'ref_id':img_id
+        }
+        _id = r.xadd('camera:0:facedect',msg)
+
+    #name = 'outputtest'+str(count)+'.jpg'
+    #cv2.imwrite(name,arr)
+    #count += 1
 
